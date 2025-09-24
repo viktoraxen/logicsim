c231277a856492ad412845f57bbfbe994392cf21
Viktor Axén <vikax460@student.liu.se>
2025-09-24 22:10:08 +0200

    added Dlatch, SRlatch tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,16 @@
 
 int main(int argc, char* argv[])
 {
-    Node D, E, Q, q, A, B, C;
+    Node S, R, Q, q;
 
-    auto n = Gate::create<NotGate>(D, A);
-    auto a1 = Gate::create<AndGate>(A, E, B),
-         a2 = Gate::create<AndGate>(D, E, C);
-    auto n1 = Gate::create<NorGate>(B, q, Q),
-         n2 = Gate::create<NorGate>(C, Q, q);
+    // Create the SR latch logic using cross-coupled NOR gates
+    auto n1 = Gate::create<NorGate>(S, Q, q);
+    auto n2 = Gate::create<NorGate>(R, q, Q);
 
-    E.set_value(true);
-    D.set_value(true);
-
-    std::cout << "Q  " << Q.read_value() << std::endl;
-    std::cout << "q  " << q.read_value() << std::endl;
-
-    D.set_value(false);
-
-    std::cout << "Q  " << Q.read_value() << std::endl;
-    std::cout << "q  " << q.read_value() << std::endl;
+    std::cout << "S " << S.read_value() << std::endl;
+    std::cout << "R " << R.read_value() << std::endl;
+    std::cout << "Q " << Q.read_value() << std::endl;
+    std::cout << "q " << q.read_value() << std::endl;
 
     return 0;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,71 @@
 #include <gtest/gtest.h>
 
-// TODO: Change test name
-TEST(Scheduler, DLatch)
+#include "gate.hpp"
+#include "node.hpp"
+
+TEST(Latches, D)
 {
-    EXPECT_EQ(1, 1);
+    Node D, E, Q, q, A, B, C;
+
+    auto n = Gate::create<NotGate>(D, A);
+    auto a1 = Gate::create<AndGate>(A, E, B),
+         a2 = Gate::create<AndGate>(D, E, C);
+    auto n1 = Gate::create<NorGate>(B, q, Q),
+         n2 = Gate::create<NorGate>(C, Q, q);
+
+    // E high->Q = D, q = !D
+    E.set_value(true);
+    D.set_value(true);
+
+    EXPECT_EQ(Q.read_value(), true);
+    EXPECT_EQ(q.read_value(), false);
+
+    D.set_value(false);
+
+    EXPECT_EQ(Q.read_value(), false);
+    EXPECT_EQ(q.read_value(), true);
+
+    // E low->Q, q does not change value
+    E.set_value(false);
+
+    EXPECT_EQ(Q.read_value(), false);
+    EXPECT_EQ(q.read_value(), true);
+
+    D.set_value(true);
+
+    EXPECT_EQ(Q.read_value(), false);
+    EXPECT_EQ(q.read_value(), true);
+}
+
+TEST(Latches, SR)
+{
+    Node S, R, Q, q;
+
+    auto n1 = Gate::create<NorGate>(S, Q, q);
+    auto n2 = Gate::create<NorGate>(R, q, Q);
+
+    // S high -> Set
+    S.set_value(true);
+
+    EXPECT_EQ(Q.read_value(), true);
+    EXPECT_EQ(q.read_value(), false);
+
+    // S, R low -> Hold previous
+    S.set_value(false);
+
+    EXPECT_EQ(Q.read_value(), true);
+    EXPECT_EQ(q.read_value(), false);
+
+    // R high -> Reset
+    S.set_value(false);
+    R.set_value(true);
+
+    EXPECT_EQ(Q.read_value(), false);
+    EXPECT_EQ(q.read_value(), true);
+
+    // S, R low -> Hold previous
+    R.set_value(false);
+
+    EXPECT_EQ(Q.read_value(), false);
+    EXPECT_EQ(q.read_value(), true);
 }
